7f3889a8dd6938acb676f505a492c66d30e5ea8d
mmorales4050 <mmorales4050@gmail.com>
2019-03-25 15:36:58 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+image_number = 0
+
+20.times do
+  Category.create(name: Faker::Commerce.department, description: Faker::Marketing.buzzwords, image_url: "https://picsum.photos/300/300/?image#{image_number}")
+  image_number += 1
+end
+
+400.times do
+  Product.create(name: Faker::Commerce.product_name, price: rand(5.00...500.00), image_url: "https://picsum.photos/300/300/?image#{image_number}", description: Faker::Marketing.buzzwords)
+  image_number += 1
+end
+
+category_number = 1
+product_number = 1
+count = 0
+400.times do
+  CategoryProduct.create(category_id: category_number, product_id: product_number)
+  product_number += 1
+  count += 1
+  if count == 20
+    count = 0
+    category_number += 1
+  end
+end

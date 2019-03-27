5bf2fbe4ea87961badf62b11b814f35bc0251c7d
mmorales4050 <mmorales4050@gmail.com>
2019-03-27 17:23:21 -0400

    added description and shipping time to products

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'categories#index'
   post '/cart', to: 'carts#remove_item', as: 'remove_item'
+
 end

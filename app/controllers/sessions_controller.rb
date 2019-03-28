b0baf1c428ec5fe59d8752f04f35c8443d065e32
seanlucius <seanlucius22@icloud.com>
2019-03-28 10:29:11 -0400

    add user functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to root_path
+    end
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      log_in user
+      params[:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or(user)
+    else
+      flash.now[:danger] = "Invalid email or password!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
